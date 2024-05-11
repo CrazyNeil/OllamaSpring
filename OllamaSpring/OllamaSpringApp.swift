a9b0808e52e08fbe671546b8d7b8a594e66a27b3
neil <daiyuechuan@outlook.com>
2024-05-11 10:58:36 +0800

    Initial Commit

---
+++ OllamaSpring/OllamaSpringApp.swift
@@ -0,0 +1,17 @@
+//
+//  OllamaSpringApp.swift
+//  OllamaSpring
+//
+//  Created by NeilStudio on 2024/5/11.
+//
+
+import SwiftUI
+
+@main
+struct OllamaSpringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
